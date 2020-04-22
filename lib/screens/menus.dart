ab1eb1e8cfb8866c3f0e39e891a477d32aa6496b
Arkar Min Tun <arkarmintun1@gmail.com>
2020-04-22 23:29:51 +0630

    menus and shops added

---
+++ lib/screens/menus.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Menus extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/shops.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Shops extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
